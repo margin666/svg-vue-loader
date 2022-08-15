a05a6dc57e5902bf9ae982b53012ffd15ec24e07
Tony Teacher <margin666@163.com>
2022-08-15 17:30:43 +0800

    fix: 修复图片设置固定颜色，不能自定义颜色

--- packages/svg-vue-loader.js
+++ packages/svg-vue-loader.js
@@ -1,6 +1,8 @@
 function fn(source, map) {
+    // 使用正则替换那些已经设置了颜色的svg图片，使其可以跟随容器颜色
+    const result = source.replace(/fill\s?=\s?(['"])(.+?)\1/g, 'fill="currentColor"')
     const tem = `<template>
-                    <div :style="{width: size+'px', height: size+'px', color: color, overflow: 'hidden'}">${source}</div>
+                    <div :style="{width: size+'px', height: size+'px', color: color, overflow: 'hidden'}">${result}</div>
                 </template>
                 <script setup>
                     defineProps({
